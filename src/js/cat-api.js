968aa8d254ae5b71b17f5eb4151ef734dfddb88f
Dima-1990 <99199420+Dima-1990@users.noreply.github.com>
2023-06-19 20:38:59 +0300

    key+

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,21 +1,23 @@
 
 export function fetchBreeds() {
-    return fetch('https://api.thecatapi.com/v1/breeds?api_key=live_LE7LQl5ugcBdlFBqIlzhAfTa2ZZblix48xFhVb3qoUJjHWTkjCgaTaX0JZaZ2aDy').then(data => {
-        if (!data.ok) {
-            throw new Error(data.status)
-        }
-        return data.json();
-    })
+    return fetch(
+      'https://api.thecatapi.com/v1/breeds?api_key=live_7EQcmdc6cjfAS5dGTnZaYoR5ykk0CvHNDHPu4nFne8uuHbiKYW4e8Nl2qma0Nz5H'
+    ).then(data => {
+      if (!data.ok) {
+        throw new Error(data.status);
+      }
+      return data.json();
+    });
 };
 
  export function fetchCatByBreed(breedId) {
     
-     return fetch(`https://api.thecatapi.com/v1/images/search?has_breeds=1&breed_ids=${breedId}&api_key=live_LE7LQl5ugcBdlFBqIlzhAfTa2ZZblix48xFhVb3qoUJjHWTkjCgaTaX0JZaZ2aDy`)
-      
-         .then(data => {
-             if (!data.ok) {
-                 throw new Error(data.status)
-             }
-             return data.json();
-         });
+     return fetch(
+       `https://api.thecatapi.com/v1/images/search?has_breeds=1&breed_ids=${breedId}&api_key=live_7EQcmdc6cjfAS5dGTnZaYoR5ykk0CvHNDHPu4nFne8uuHbiKYW4e8Nl2qma0Nz5H`
+     ).then(data => {
+       if (!data.ok) {
+         throw new Error(data.status);
+       }
+       return data.json();
+     });
 }; 
